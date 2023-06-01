328d80f4e1980e2804a06ed2601d65578e6a589d
Edgar Nwajei <edgarlinked@gmail.com>
2023-06-01 12:53:04 +0100

    updated stuff

--- index.js
+++ index.js
@@ -3,3 +3,35 @@ const score = document.getElementById('score');
 const result = document.getElementById('result');
 const restart = document.getElementById('restart');
 const modal = document.querySelector('.modal');
+
+const scoreboard = {
+    player: 0,
+    computer: 0
+}
+
+//play
+function play(e) {
+  const playerChoice = e.target.id;
+  const computerChoice = getComputerChoice();
+console.log(playerChoice, computerChoice);
+//computers choice
+function getComputerChoice() {
+  const rand = Math.random();
+if (rand < 0.34) {
+    return 'rock';
+  } else if (rand <= 0.67) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+}
+
+
+
+
+}
+
+// events
+
+choices.forEach(choice => choice.addEventListener('click', play));
+
